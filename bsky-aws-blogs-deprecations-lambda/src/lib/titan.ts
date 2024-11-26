de40fd3b1c1d877f8146b686380c1c97263883cf
Ernyoke <ernyoke19930115@gmail.com>
2024-11-26 09:46:47 +0200

    Implemented langchain message trimmer.

--- bsky-aws-blogs-deprecations-lambda/src/lib/titan.ts
+++ bsky-aws-blogs-deprecations-lambda/src/lib/titan.ts
@@ -4,8 +4,7 @@ import {BaseOutputParser} from "@langchain/core/output_parsers";
 import {config} from "./config.js";
 import {dedent} from "ts-dedent";
 import {Logger} from "@aws-lambda-powertools/logger";
-import _ from 'lodash';
-import {RunnableSequence} from "@langchain/core/runnables";
+import {BaseMessage, trimMessages} from "@langchain/core/messages";
 
 const maxTokenSize = config.titanMaxTokenSize;
 
@@ -44,7 +43,7 @@ export class TitanModel {
             deprecation of an AWS service, product, or any related feature. Examples of such services include EC2, ECS, S3, 
             and CodeDeploy, among others. If you identify a blog post mentioning the deprecation of any such service or product, 
             you must respond with a list of all the services or products that are deprecated.`;
-    chat = ChatPromptTemplate.fromMessages([
+    chatMessages = ChatPromptTemplate.fromMessages([
         [
             'system',
             '{systemPrompt}\n{formatInstructions}'
@@ -54,6 +53,11 @@ export class TitanModel {
             dedent`Blog title: {title}
             Blog content: {content}`
         ],
+        // Required because the trimmer tends to drop the last message from the chat from some reason
+        [
+            'human',
+            'END'
+        ]
     ]);
 
     private model = new BedrockChat({
@@ -66,54 +70,42 @@ export class TitanModel {
         }
     });
 
-    private parser: YesNoOutputParser;
+    private readonly parser: YesNoOutputParser;
 
     constructor(private logger: Logger) {
         this.parser = new YesNoOutputParser(this.logger);
     }
 
     async checkIfArticleIsAboutDeprecation(title: string, content: string) {
-        content = await this.fitContentIntoTokenWindow(title, content);
-
-        const chain = RunnableSequence.from([
-            this.chat,
-            this.model,
-            this.parser
-        ]);
-
-        return await chain.invoke({
-            systemPrompt: this.systemPrompt,
-            title: title,
-            content: content,
-            formatInstructions: this.parser.getFormatInstructions()
-        });
-    }
-
-    async fitContentIntoTokenWindow(title: string, content: string): Promise<string> {
-        const messages = await this.chat.format({
-            systemPrompt: this.systemPrompt,
-            title: title,
-            content: content,
-            formatInstructions: this.parser.getFormatInstructions()
+        const trimmer = trimMessages({
+            maxTokens: maxTokenSize,
+            tokenCounter: async (messages: BaseMessage[]) => {
+                const nrTokens = await this.model.getNumTokens(
+                    messages
+                        .map(message => message.content)
+                        .join('')
+                );
+                this.logger.debug(`Tokens size ${nrTokens}`);
+                return nrTokens;
+            },
+            strategy: "first",
+            allowPartial: true,
         });
 
-        const nrTokens = await this.model.getNumTokens(messages);
-        this.logger.info(`Number of tokens for the article ${title}: ${nrTokens}`);
+        const chain = trimmer
+            .pipe(this.model)
+            .pipe(this.parser);
 
-        const charTokenRatio = messages.length / nrTokens;
+        const messages = await this.chatMessages.formatMessages(
+            {
+                systemPrompt: this.systemPrompt,
+                title: title,
+                content: content,
+                formatInstructions: this.parser.getFormatInstructions(),
+            }
+        )
 
-        if (nrTokens > maxTokenSize) {
-            const nrTokensSystemPrompt = await this.model.getNumTokens(this.systemPrompt + this.parser.getFormatInstructions() + 1);
-            const maxNrTokensAllowed = maxTokenSize - nrTokensSystemPrompt;
-            const currentNrTokensContent = nrTokens - nrTokensSystemPrompt;
-            const charsToCut = _.toInteger((currentNrTokensContent - maxNrTokensAllowed) * charTokenRatio + 1);
-            this.logger.info(`Number of tokens for the article ${title} does not fit into ${maxTokenSize} tokens allowed. Cut ${charsToCut} characters.`);
-            return content.substring(0, content.length - title.length - charsToCut - 1);
-        } else {
-            this.logger.info(`Number of tokens for the article ${title} fits into the window of ${maxTokenSize} tokens`);
-        }
-
-        return content;
+        return await chain.invoke(messages);
     }
 }
 
